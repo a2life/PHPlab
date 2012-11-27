a9020d60248241b8a99a8118d35385558a29a7d1
a2life.shogishack.net <a2life.shogishack.net>
2012-11-27 18:45:50 -0500

    added logic to parse sente and gote mochigoma (incomplete)

--- readInit.php
+++ readInit.php
@@ -7,17 +7,24 @@
  * To change this template use File | Settings | File Templates.
  * This file will find kifu position data and parse
  */
-$init_data=""; $init_array=array();
+//$init_data=""; $init_array=array();
 include_once "inc/initdata.inc.php";
 include_once "inc/findline.php";
+$convertArray=array("p"=>"歩","P"=>"と",'L'=>"成香|杏","l"=>"香",'N'=>'成桂|圭',
+    'n'=>'桂','S'=>'成銀|全','s'=>'銀','r'=>'飛',"R"=>"[竜龍]","b"=>"角","B"=>"馬","k"=>"玉","g"=>"金",","=>"\s");
+$boardMarker="９ ８ ７ ６ ５ ４ ３ ２ １";
+$senteOnHand="\n先手の持駒：(\w*)\s*\n";
+$goteOnHand= "\n後手の持駒：(\w*)\s*\n";
+$onHands=array();
+if(mb_ereg($senteOnHand,$init_data,$onHands)!=false)$senteOnHand=$onHands[1];
+if(mb_ereg($goteOnHand,$init_data,$onHands)!=false)$goteOnHand=$onHands[1];
 
 $init_array=explode("\r\n",$init_data);
-$c=count($init_array);
-echo $c."\n";
-$i= findline("９ ８ ７ ６ ５ ４ ３ ２ １",$init_array);
-if ($i==true){
-$i=$i+2;  //start of 局面　info
-$j=$i+9;  //end of 局面　info
+$i= findline($boardMarker,$init_array);
+if ($i==true){ //the string contains board chart
+
+    $i=$i+2;  //starting row of 局面　info
+$j=$i+9;  //ending row of 局面　info
  for ($n=$i;$n<$j;$n++){
     $m= mb_strlen($init_array[$n]);
      for($k=0;$k<$m;$k++){
@@ -25,7 +32,7 @@ $j=$i+9;  //end of 局面　info
      }
      echo "\n";
  }
-$SenteOnBoard=""; $goteOnBoard="";
+$senteOnBoard=""; $goteOnBoard="";
 for ($row=$i;$row<$j;$row++){
     //$columnLength=mb_strlen($init_array[$row]);
     for ($k=2;$k<19;$k=$k+2){
@@ -35,13 +42,30 @@ for ($row=$i;$row<$j;$row++){
             $side=mb_substr($init_array[$row],$k-1,1);
 
             switch($side){
-                 case " ": $SenteOnBoard.=$colRow; $SenteOnBoard.=$masu; $SenteOnBoard.=" "; break;
-                 case "v": $goteOnBoard.=$colRow; $goteOnBoard.=$masu;$goteOnBoard.=" ";break;
+                 case " "://This is Sente's piece
+                     $senteOnBoard.=$colRow;
+                     $senteOnBoard.=$masu;
+                     $senteOnBoard.=" ";
+                     break;
+                 case "v"://this is Gote's piece
+                     $goteOnBoard.=$colRow;
+                     $goteOnBoard.=$masu;
+                     $goteOnBoard.=" "
+                     ;break;
             }
         }
     }
 }
 
-var_dump($SenteOnBoard);
-var_dump($goteOnBoard);
+    $senteOnBoard=trim($senteOnBoard);
+    $goteOnBoard=trim($goteOnBoard);
+
+foreach ($convertArray as $replacement=>$pattern){
+    $senteOnBoard=mb_ereg_replace($pattern,$replacement,$senteOnBoard);
+    $goteOnBoard=mb_ereg_replace($pattern,$replacement,$goteOnBoard);
 }
+
+var_dump($senteOnBoard);
+var_dump($goteOnBoard);
+}var_dump($senteOnHand);
+var_dump($goteOnHand);
